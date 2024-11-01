2663bf68ee9d078b889f602d8fe322a038dbd49e
songs <songshu0905@gmail.com>
2024-11-01 21:34:52 +0800

    fix: convert 6dof

--- convert_6dof.py
+++ convert_6dof.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 import transformations as tf
+from convert_stereo_data.convert_extrinsic import T_body_imu
 
 # 定义文件路径
-slampose_file = '/home/songshu/datasets/2020-08-14/slampose/data.csv'
-output_file = '/home/songshu/datasets/2020-08-14/slampose/6dof_data.csv'
+slampose_file = '/home/roborock/datasets/roborock/stereo/2020-08-14/slampose/data.csv'
+output_file = '/home/roborock/datasets/roborock/stereo/2020-08-14/slampose/imu_data.csv'
 
 # 定义 slam pose 数据的列名
 slampose_columns = [
@@ -29,7 +30,7 @@ slampose_df = pd.read_csv(
 
 # 查看数据是否成功读取
 print("slam pose 数据预览:")
-print(slampose_df.tail(10))
+print(slampose_df.tail(20))
 
 # 将 slam pose 数据转换为 6DOF 数据
 # 6DOF 数据的列名
@@ -47,6 +48,20 @@ output_columns = [
 # 初始化输出数据
 output_data = []
 
+# convert frame from body to imu
+# T_imu_body:
+# [[-0.     -1.     -0.     -0.    ]
+#  [ 1.      0.      0.     -0.104 ]
+# [ 0.      0.      1.     -0.0162]
+# [ 0.      0.      0.      1.    ]]
+T_imu_body = np.array([
+    [0.0, -1.0, 0.0, 0],
+    [1.0, 0.0, 0.0, -0.104],
+    [0.0, 0.0, 1.0, -0.0162],
+    [0.0, 0.0, 0.0, 1.0]])
+T_body_imu = np.linalg.inv(T_imu_body)
+print(T_body_imu)
+
 # 逐行处理 slam pose 数据
 for index, row in slampose_df.iterrows():
     # 读取每一行的数据
@@ -57,18 +72,21 @@ for index, row in slampose_df.iterrows():
 
     # 计算 6DOF 数据
     # 3x3 rotation matrix
-    R = tf.rotation_matrix(pose_yaw, [0, 0, 1])[:3, :3]
+    R = tf.euler_matrix(0, 0, pose_yaw)[:3, :3]
     M = np.eye(4)
     M[:3, :3] = R
+    M[:3, 3] = [pose_x, pose_y, 0]
+    M = np.dot(T_imu_body, M)
+    M = np.dot(M, T_body_imu)
     # convert R to quaternion
     quat = tf.quaternion_from_matrix(M)
 
     # 将数据添加到输出数据中
     output_data.append([
         timestamp,
-        pose_x,
-        pose_y,
-        0.0,
+        M[0, 3],
+        M[1, 3],
+        M[2, 3],
         quat[3],
         quat[0],
         quat[1],
@@ -80,7 +98,7 @@ output_df = pd.DataFrame(output_data, columns=output_columns)
 
 # 查看最终输出的数据(最后10行)
 print("\n最终输出的数据预览:")
-print(output_df.tail(10))
+print(output_df.tail(20))
 
 # 将整合后的数据输出到 CSV 文件，以空格分隔, header以#开头
 output_df.to_csv(output_file, index=False, sep=',', header=True, float_format='%.6f')
